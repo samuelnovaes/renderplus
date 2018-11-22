b3ad835d4d0a650faa5c6186563de7480f4ad5df
Samuel Novaes <samuel.novaes.96@gmail.com>
2018-11-22 05:19:52 -0200

    Perform

--- renderplus.js
+++ renderplus.js
@@ -1,50 +1,34 @@
-function camel2Kebab(txt) {
-	return txt.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
-}
-
 module.exports = (req, res, next) => {
-	res.render = (elem) => {
-		function convert(arr) {
-			if (!Array.isArray(arr)) return arr
+	res.render = elem => {
+		const m = arr => {
+			if (!Array.isArray(arr)) return (arr || '')
 
-			let autoclose = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'menuitem', 'meta', 'param', 'source', 'track', 'wbr']
-			let tag = arr[0]
-			let props = arr[1]
-			let children = arr[2]
-			let els = arr[3]
+			const tag = arr[0]
 
-			if (tag == 'for') return props.map(children).map(elem => convert(elem)).join('')
+			if (tag == 'for') return arr[1].map(elem => m(arr[2](elem))).join('')
 
 			if (tag == 'if') {
-				if (props) {
-					return convert(children)
-				}
-				if (els) {
-					return convert(els)
+				if (arr[2]) {
+					if (arr[1]) return m(arr[2].then)
+					return m(arr[2].else)
 				}
 				return ''
 			}
 
-			if (Array.isArray(props) || typeof props != 'object') {
-				children = props
-				props = {}
-			}
+			const autoclose = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'menuitem', 'meta', 'param', 'source', 'track', 'wbr']
+			const noProps = typeof arr[1] != 'object' || Array.isArray(arr[1])
+			const props = noProps ? {} : arr[1]
+			const children = (noProps ? arr[1] : arr[2]) || []
+			const childrenArray = Array.isArray(children) ? children : [children]
+			const strChildren = childrenArray.map(c => m(c)).join('')
+			const camel2Kebab = txt => txt.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase()
+			const strProps = Object.entries(props).map(e => ` ${camel2Kebab(e[0])}${e[1] === '' ? '' : `="${e[1]}"`}`).join('')
 
-			props = props || {}
-			children = children || []
+			if (tag) return `<${tag}${strProps}>${!autoclose.includes(tag) ? `${strChildren}</${tag}>` : ''}`
 
-			children = Array.isArray(children) ? children : [children]
-			let strChildren = children.map(c => convert(c)).join('')
-
-			let strProps = ''
-			for (let i in props) {
-				strProps += ` ${camel2Kebab(i)}${props[i] === '' ? '' : `="${props[i]}"`}`
-			}
-
-			if (tag) return `<${tag}${strProps}>${autoclose.indexOf(tag) == -1 ? `${strChildren}</${tag}>` : ''}`
 			return ''
 		}
-		res.send(`<!DOCTYPE html>${convert(elem)}`)
+		res.send(`<!DOCTYPE html>${m(elem)}`)
 	}
 	next()
 }
